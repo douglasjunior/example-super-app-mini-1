97a412b9dd9e521a1ac7a35d299850964bc3097f
Douglas Nassif Roma Junior <douglas.junior@db1.com.br>
2022-06-27 17:34:13 -0300

    implementa um subscribe do EventEmitter

--- lib/domains/screen2/Screen2.tsx
+++ lib/domains/screen2/Screen2.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import {Button, View} from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
+import {emitter} from 'example-super-app-context';
 import {useNavigation} from '../../hooks/useNavigation';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  counterButton: {
+    marginTop: 16,
+  },
+});
 
 const Screen2 = () => {
   const navigation = useNavigation();
   return (
-    <View style={{flex: 1}}>
+    <View style={styles.container}>
       <Button
         title="Voltar"
         onPress={() => {
           navigation.goBack();
         }}
       />
+      <View style={styles.counterButton}>
+        <Button
+          title="Incrementar contador"
+          onPress={() => {
+            emitter.emit('INCREMENT_MESSAGE', 1);
+          }}
+        />
+      </View>
     </View>
   );
 };
